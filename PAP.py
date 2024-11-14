9282a1e73ef8b6b66fa188b8e4379bdf4c7589fb
LB-Ashura <fvashura@gmail.com>
2024-11-14 09:51:50 -0800

    Create PAP.py

---
+++ PAP.py
@@ -0,0 +1,14 @@
+import tkinter as tk 
+MCPAP = tk.TK()
+MCPAP.geometry("800 x 720") 
+TitleIMG = tk.
+TitleLabel = tk.label(MCPAP, text="Minecraft Pick-A-Path")
+TitleLabel.pack()
+
+StartB = tk.button(MCPAP, text="Press to Start")
+StartB.pack()
+
+
+
+
+MCPAP.mainloop()
