8fa35f15989e6bfae3f5ec014ec97a31826bdccf
LB-Ashura <fvashura@gmail.com>
2024-11-14 10:09:47 -0800

    Update PAP.py

--- PAP.py
+++ PAP.py
@@ -8,7 +8,13 @@ TitleLabel.pack()
 StartB = tk.button(MCPAP, text="Press to Start")
 StartB.pack()
 
+P1Label = tk.label(MCPAP, text="You entered a cave you haven't explored before\nThere are two paths awaiting you\nDo you go left or right?")
+P1Label.pack()
 
+P1B1 = tk.button(MCPAP, text="Go left")
+P1B1.pack()
 
+P1B2 = tk.button(MCPAP, text="Go right")
+P1B2.pack()
 
 MCPAP.mainloop()
