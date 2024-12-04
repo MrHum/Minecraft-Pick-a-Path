656674fc6cdd4986be3a0ee495e4bf58c42aa913
LB-Ashura <fvashura@gmail.com>
2024-12-04 11:39:26 -0800

    Update PAP.py

--- PAP.py
+++ PAP.py
@@ -11,10 +11,25 @@ StartB.pack()
 P1Label = tk.label(MCPAP, text="You entered a cave you haven't explored before\nThere are two paths awaiting you\nDo you go left or right?")
 P1Label.pack()
 
-P1B1 = tk.button(MCPAP, text="Go left")
+Def P1B1Click():
+  print("You take 10 steps into the left path, you don’t pay attention and end up falling into lava.\nNice one.")
+
+P1B1 = tk.button(MCPAP, text="Go into the left", command=P1B1Click)
 P1B1.pack()
 
-P1B2 = tk.button(MCPAP, text="Go right")
+Def P1B2Click():
+  print( "You enter the right path and end up bumping into some light from lava far ahead./nYou see that there are again 2 paths, up or down next to a patch of lava illuminating the area.")	
+
+P1B2 = tk.button(MCPAP, text="Go into the right", command=P1B2Click)
 P1B2.pack()
 
+Def P2B1Click(): 
+  print("You decide to move upwards and don’t see any mobs. You end up moving more forward and you see a skeleton by itself."
+
+
+
+
+
+
+P2Label = tk.label(MCPAP, text="
 MCPAP.mainloop()
